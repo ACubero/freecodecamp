26415fb894c5726285f24c17650c4b550ea1d8a7
ACubero <alexcuar@gmail.com>
2024-03-01 21:56:51 +0100

    Como declarar variables

---
+++ Curso JavaScript/Variables/script.js
@@ -0,0 +1,8 @@
+/*
+Tipos de datos:
+undefined, null, boolean, string, symbol, number y object
+*/
+var miVariable = "freeCodeCamp";
+console.log(miVariable);
+miVariable = 16;
+console.log(miVariable);
