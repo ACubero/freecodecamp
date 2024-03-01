7a559fb1ed0d21127b8f42f2225af5d9cad34535
ACubero <alexcuar@gmail.com>
2024-03-01 22:32:48 +0100

    Ajustes en las carpetas y ficheros y añadir la carpeta de asignacion

--- Curso JavaScript/02. Variables/script.js
+++ Curso JavaScript/02. Variables/script.js
@@ -2,7 +2,9 @@
 Tipos de datos:
 undefined, null, boolean, string, symbol, number y object
 */
+var a;
+a= 2;
 var miVariable = "freeCodeCamp";
 console.log(miVariable);
 miVariable = 16;
-console.log(miVariable);
+console.log(a);
---
+++ Curso JavaScript/03. AsignacionValores/script.js
@@ -0,0 +1,48 @@
+/*
+Tipos de datos:
+undefined, null, boolean, string, symbol, number y object
+*/
+var a;
+var b =2;
+
+console.log(a);
+console.log(b);
+
+var producto = 3.4 * 10.4
+console.log(producto);
+
+
+var salario = 45;
+console.log(salario);
+
+salario *= 5;
+
+console.log(salario);
+for (let for (const key in object) {
+    if (Object.hasOwnProperty.call(object, key)) {
+        const element = object[key];
+        index
+    }
+} = 0; for (const key in object) {
+    if (Object.hasOwnProperty.call(object, key)) {
+        const element = object[key];
+        index
+    }
+} < array.length; for (const key in object) {
+    if (Object.hasOwnProperty.call(object, key)) {
+        const element = object[key];
+        index
+    }
+}++) {
+    const element = array[for (const key in object) {
+        if (Object.hasOwnProperty.call(object, key)) {
+            const element = object[key];
+            index
+        }
+    }];
+    
+}
+
+if (complemento) {
+    
+}
