ddba2c4681601b5033a15fbce6757c4aa24720ef
@alexcuar <alexcuar@gmail.com>
2024-03-02 13:42:05 +0100

    Cadenas de caracteres

--- Curso JavaScript/04_CadenasCaracteres/script.js
+++ Curso JavaScript/04_CadenasCaracteres/script.js
@@ -1,27 +1,109 @@
-/*
-Tipos de datos:
-undefined, null, boolean, string, symbol, number y object
+var nombre = "Alan";
+var nombreDos = 'Miguel Angel';
+
+console.log(nombre);
+console.log(nombreDos);
+
+// Escapar comillas
+var miCadena ="Soy una cadena de caracteres \"con comillas\"";
+console.log(miCadena);
+
+var miMeta;
+miMeta = 'Soy una cadena de caracteres "con comillas"';
+console.log(miMeta);
+
+/*Secuencias de escape
+
+código | Resultado
+--------------------
+\'      Comilla Simple
+\"      Comilla doble
+\\      Barra invertida
+\n      Línea Nueva
+\r      Retorno de carro
+\t      Tabulación
+\b      Retroceso
+\f      Salto de Página
 */
-var a;
-var b =2;
 
-console.log(a);
-console.log(b);
+console.log("Cadena de texto con \nlínea nueva");
+console.log("Cadena de texto con \ttabulador \ttabulador2");
 
-var producto = 3.4 * 10.4
-console.log(producto);
+//Concatenar cadenas
+var nombreCompleto = "Alan" +" "+ "Turing";
+console.log(nombreCompleto);
 
+//Construir cadenas con variables
+var verbo = "programar";
+var mensaje = "Estoy aprendiendo a "+verbo + " con freecodecamp";
+console.log(mensaje);
 
-var salario = 45;
-console.log(salario);
+//Agregar variables a cadenas de caracteres
+var mensajeCompleto = "Estoy aprendiendo a programar ";
+var parteFinal = "con freecodecamp";
+console.log(mensajeCompleto);
 
-salario *= 5; // salario = salario * 45
+mensajeCompleto += parteFinal;
+console.log(mensajeCompleto);
 
-console.log(salario);
+//Longitud de una cadena de caracteres
+var miCadena;
+miCadena = "Estoy aprendiendo a programas JavaScript ";
+console.log("Número de caracteres: "+miCadena.length);
 
-var salario = 45000;
-console.log(salario);
+//Notación de corchetes: Primer carácter -- Comienza por 0
+var lenguajeDeProgramacion = "JavaScript";
+console.log(lenguajeDeProgramacion[0]);
 
-salario /= 2; // salario = salario / 2
+// Inmutabilidad de Cadenas de Caracteres
+var miCadena = "Jola, Mundo";
+console.log(miCadena);
+miCadena[0] = "H"; // no se puede cambiar, solo se puede cambiar todo el texto
+miCadena = "Hola, Mundo";
+console.log(miCadena);
 
-console.log(salario);
+// Notación de corchetes: enésimo carácter
+var miCadena = "JavaScript";
+console.log(miCadena[0]);
+console.log(miCadena[1]);
+console.log(miCadena[2]);
+console.log(miCadena[3]);
+console.log(miCadena[4]);
+console.log(miCadena[5]);
+console.log(miCadena[6]);
+console.log(miCadena[7]);
+console.log(miCadena[8]);
+console.log(miCadena[9]);
+console.log(miCadena[10]); //NO da error pero devuelve undefined
+console.log(miCadena[-1]); //NO da error pero devuelve undefined
+
+// Notación de Corchetes: ultimo caracter
+var miCadena;
+//El último indice siempre es longitud del texto - 1
+miCadena = "JavaScript";
+console.log(miCadena[miCadena.length - 1]);
+miCadena = "Python";
+console.log(miCadena[miCadena.length - 1]);
+
+// Notación de Cochetes: de izquierda a derecha
+var miCadena;
+var n;
+miCadena = "JavaScript";
+n = 4;
+console.log(miCadena[miCadena.length - n]);
+
+// Palabras en blanco
+var miSustantivo = "perro";
+var miAdjetivo = "negro";
+var miVerbo = "corrió";
+var miAdverbio = "rápidamente";
+
+var palabrasEnBlanco = "El "+miSustantivo+" "+miVerbo+" "+miAdverbio+" a la tienda";
+console.log(palabrasEnBlanco);
+
+var miSustantivo = "bicicleta";
+var miAdjetivo = "pequeña";
+var miVerbo = "voló";
+var miAdverbio = "rápidamente";
+var palabrasEnBlanco = "La "+miSustantivo+" "+miVerbo+" "+miAdverbio+" a la tienda";
+console.log(palabrasEnBlanco);
