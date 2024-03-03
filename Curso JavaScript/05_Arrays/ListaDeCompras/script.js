13d354aa4a6d996c7106ef546fd9d81622dff656
@alexcuar <alexcuar@gmail.com>
2024-03-03 08:53:19 +0100

    Proyecto para acceder a un array anidado

---
+++ Curso JavaScript/05_Arrays/ListaDeCompras/script.js
@@ -0,0 +1,8 @@
+
+var miListaDeCompras =  [["cereal",3],["galletas",4],["pan",5],["refesco",7],["pollo",7]]
+
+console.log("Voy a comprar "+miListaDeCompras[0][1]+" unidades de "+miListaDeCompras[0][0]);
+console.log("Voy a comprar "+miListaDeCompras[1][1]+" unidades de "+miListaDeCompras[1][0]);
+console.log("Voy a comprar "+miListaDeCompras[2][1]+" unidades de "+miListaDeCompras[2][0]);
+console.log("Voy a comprar "+miListaDeCompras[3][1]+" unidades de "+miListaDeCompras[3][0]);
+console.log("Voy a comprar "+miListaDeCompras[4][1]+" unidades de "+miListaDeCompras[4][0]);
